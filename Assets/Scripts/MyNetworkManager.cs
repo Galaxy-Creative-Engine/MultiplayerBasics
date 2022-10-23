76fe06003889a6ac4d00ae20bfe43a1b120ef898
Shaeen Singh <shaeendev@gmail.com>
2022-10-23 08:16:12 +0200

    Creating a custom network manager

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager {
+    public override void OnClientConnect(NetworkConnection conn) {
+        base.OnClientConnect(conn);
+
+        Debug.Log("I connected to a server!");
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn) {
+        base.OnServerAddPlayer(conn);
+
+        Debug.Log($"Lobby total: {numPlayers}");   
+    }
+}
