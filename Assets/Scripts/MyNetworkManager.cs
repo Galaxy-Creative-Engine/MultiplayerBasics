54c9a9638b1aa5cf4d39b08ac1e543b8e934c213
Shaeen Singh <shaeendev@gmail.com>
2022-10-23 08:38:41 +0200

    Syncing player vars with the network manager

--- Assets/Scripts/MyNetworkManager.cs
+++ Assets/Scripts/MyNetworkManager.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
 
-public class MyNetworkManager : NetworkManager {
-    public override void OnClientConnect(NetworkConnection conn) {
-        base.OnClientConnect(conn);
-
-        Debug.Log("I connected to a server!");
-    }
-
-    public override void OnServerAddPlayer(NetworkConnection conn) {
+public class MyNetworkManager : NetworkManager 
+{
+    
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
         base.OnServerAddPlayer(conn);
 
-        Debug.Log($"Lobby total: {numPlayers}");   
+        MyNetworkPlayer player = conn.identity.GetComponent<MyNetworkPlayer>();
+
+        player.SetDisplayName($"Player {numPlayers}");
+
+        Color displayColor = new Color(
+            Random.Range(0f, 1f),
+            Random.Range(0f, 1f),
+            Random.Range(0f, 1f)
+            );
+
+        player.SetDisplayColor(displayColor);
     }
 }
